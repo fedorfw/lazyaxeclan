1cbc025a1f6f53353c1785228d8ea16993b377fb
Fedor Khoroshilov <fedorfw25@gmail.com>
2023-02-19 01:29:53 +0300

    ой наворотил

--- views/site/about.php
+++ views/site/about.php
@@ -5,7 +5,7 @@
 use users\Domain\Entities\User;
 use users\Domain\Interfaces\UserRepositoryInterface;
 use yii\helpers\Html;
-include_once "../../telegramBot/turtleBot.php";
+include_once __DIR__."../../telegramBot/turtleBot.php";
 
 //class HandleUser
 //{
