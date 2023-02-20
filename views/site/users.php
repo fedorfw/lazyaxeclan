0721858c70889cb34fdbcdfe75710f7225c9382c
Fedor Khoroshilov <fedorfw25@gmail.com>
2023-02-20 22:08:04 +0300

    refactor telegram module

--- views/site/users.php
+++ views/site/users.php
@@ -5,6 +5,8 @@ use League\Fractal\Manager;
 use League\Fractal\Resource\Collection;
 use users\app\transformers\UserTransformer;
 use users\Domain\Interfaces\UserRepositoryInterface;
+use Yii;
+use app\modules\common\components\BaseApiController;
 
 try {
     $users = Yii::$container->get(UserRepositoryInterface::class)
