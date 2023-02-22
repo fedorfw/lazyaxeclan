779143f6d48e735deeb46ff0af45ec6923d97200
Fedor Khoroshilov <fedorfw25@gmail.com>
2023-02-23 02:26:27 +0300

    add some vue

--- views/site/index.php
+++ views/site/index.php
@@ -25,7 +25,7 @@ var index = new Vue({
   methods: {
       setFfw(){
           this.user = $.getJSON({
-            url: '/modules/users/user/list'
+            url: '/web/users/user/list'
           }).done(function (data){
               console.log(data);
           });
