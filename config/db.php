57d7905604131153cb29d36ec6ea19f1287bd368
Fedor Khoroshilov <fedorfw25@gmail.com>
2023-02-24 23:31:49 +0300

    add user(add/update/delete)

--- config/db.php
+++ config/db.php
@@ -1,11 +1,18 @@
 <?php
 
 return [
+// prod
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ffwdatabase',
-    'username' => 'root',
-    'password' => '0000',
+    'dsn' => 'mysql:host=localhost;dbname=u1942837_lazyaxeclandb',
+    'username' => 'u1942837_ffw',
+    'password' => 'dracon25',
     'charset' => 'utf8',
+// dev
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=localhost;dbname=ffwdatabase',
+//    'username' => 'root',
+//    'password' => '0000',
+//    'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
--- config/db_doctrine.php
+++ config/db_doctrine.php
@@ -1,8 +1,14 @@
 <?php
 
 return [
+// prod
     'host' => 'localhost',
-    'dbname' => 'ffwdatabase',
-    'username' => 'root',
-    'password' => '0000',
+    'dbname' => 'u1942837_lazyaxeclandb',
+    'username' => 'u1942837_ffw',
+    'password' => 'dracon25',
+// dev
+//    'host' => 'localhost',
+//    'dbname' => 'ffwdatabase',
+//    'username' => 'root',
+//    'password' => '0000',
 ];
