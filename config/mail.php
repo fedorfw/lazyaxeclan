caa760812349fdd51ff7f2a8034c71b764740922
Fedor Khoroshilov <fedorfw25@gmail.com>
2023-02-28 01:31:43 +0300

    add email service

--- config/mail.php
+++ config/mail.php
@@ -3,4 +3,12 @@
 return [
     'class' => 'yii\swiftmailer\Mailer',
     'useFileTransport' => true,
+    'transport' => [
+        'class' => 'Swift_SmtpTransport',
+        'host' => 'mail.lazyaxeclan.site',
+        'username' => 'WOW5fandg125',
+        'password' => 'WOW5fandg125',
+        'port' => '465',
+        'encryption' => 'ssl'
+    ]
 ];
