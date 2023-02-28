984a9263fa02d5a9120692bf9c5ad12680857bce
Fedor Khoroshilov <fedorfw25@gmail.com>
2023-02-28 19:41:44 +0300

    some clean

--- config/mail.php
+++ config/mail.php
@@ -4,11 +4,6 @@ return [
     'class' => 'yii\swiftmailer\Mailer',
     'useFileTransport' => false,
     'transport' => [
-        'class' => 'Swift_SmtpTransport',
-        'host' => 'mail.hosting.reg.ru',
-        'username' => 'admin@lazyaxeclan.site',
-        'password' => 'WOW5fandg125',
-        'port' => '465',
-        'encryption' => 'ssl'
+
     ]
 ];
